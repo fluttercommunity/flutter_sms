54f627adb5ea67de991642bcad924e9c171cc00e
Rody Davis <rody@rive.app>
2020-02-04 19:06:53 -0800

    removing macos

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_sms
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSmsPlugin.register(with: registry.registrar(forPlugin: "FlutterSmsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
