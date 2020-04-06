af22c5f42c822732c5300b20278845a7e8ae1289
rodydavis <rodydavis@users.noreply.github.com>
2020-04-06 02:43:29 +0000

    deploy: 060302cf8531a1572d00af16457208d9ca6f7851

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,17 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "e36211d86e9c70409c58295974f749a1",
-"/manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
-"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"/assets/LICENSE": "6382abd468771d113d5b6f5cf7657c6f",
-"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/main.dart.js": "dc77fdb5cfe0fe92137523378949776d",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796"
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/LICENSE": "99c0d690a89b8d9609ba9a47bd9c2941",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"main.dart.js": "1b3645a995654ce53861fc6e62f93ae1",
+"index.html": "e36211d86e9c70409c58295974f749a1"
 };
 
 self.addEventListener('activate', function (event) {
@@ -33,9 +33,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
